e0247acabc35a9e2b4890aa350cb2589324f5ac7
Prasad Yemekar <emekarprasad@gmail.com>
2026-02-13 15:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const timer =document.getElementByID("timer");
 
-fuction getTime(){
+function getTime(){
 	const now = new Date();
 
 	timer.textContent = now.toLocaleString();
